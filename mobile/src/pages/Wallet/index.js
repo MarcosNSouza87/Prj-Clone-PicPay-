f7187fbc475d64ea28dacda037cb23a9bf85336d
MarcosNSouza87 <31523870+MarcosNSouza87@users.noreply.github.com>
2021-01-02 23:17:11 -0300

    Add files via upload

--- mobile/src/pages/Wallet/index.js
+++ mobile/src/pages/Wallet/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View,Text } from 'react-native';
 
-import styles from './styles';
+import {Wrapper,Container,Header,Title,Saldo,
+    Bold,SubTitle,GroupBtn,BtnAdd,BtnRmv,TxtBtn} from './styles';
 
 export default function Wallet(){
     return (
-        <View style={styles.container}>
-            <View styles={styles.header}>
-                <Text style={styles.description}>wallet</Text>
-            </View>
-        </View>
+        <Wrapper >
+            <Container>
+                <Header>
+                    <Title>Saldo Picpay</Title>
+                    <Saldo>R$<Bold>0,00</Bold></Saldo>
+                    <SubTitle>
+                        Seu saldo está rendendo 100% de CDI
+                    </SubTitle>
+                    <GroupBtn>
+                        <BtnAdd onPress={()=>{}}><TxtBtn>Adicionar</TxtBtn></BtnAdd>
+                        <BtnAdd onPress={()=>{}}><TxtBtn>Remover</TxtBtn></BtnAdd>
+                    </GroupBtn>
+                </Header>
+            </Container>
+        </Wrapper>
     )
 }
