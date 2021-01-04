7da395d1ca51bca2d81bc1695fdf5e6468f0870e
MarcosNSouza87 <31523870+MarcosNSouza87@users.noreply.github.com>
2021-01-04 12:49:29 -0300

    Add files via upload

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 import { Wrapper, Container, Header,BallanceContainer,BallanceTitle,Ballance } from './styles';
 
+import Suggestions from '../../components/Suggestions'
+import Activities from '../../components/Activities'
+import Tips from '../../components/Tips'
 
 export default function Home(){
     return (
@@ -17,6 +20,9 @@ export default function Home(){
 
                     <AntDesign name="gift" size={30} color="#10c86e"/>
                 </Header>
+                <Suggestions/>
+                <Activities/>
+                <Tips/>
             </Container>
         </Wrapper>
     )
